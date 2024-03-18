d989ad466bd206944541efd78156fbe886e3a6b4
Dieter Werthmüller <mail@werthmuller.org>
2024-03-18 14:52:09 +0100

    Change shape

--- notebooks/reservoir_simulator.py
+++ notebooks/reservoir_simulator.py
@@ -23,17 +23,18 @@ class ReservoirSim:
 
     """
 
-    def __init__(self, nx, ny, **kwargs):
+    def __init__(self, perm_field, **kwargs):
         """Initialize a Simulation instance."""
         # TODO: MOVE TO MORE EXPLICIT INPUTS, NOT ONLY KWARGS
 
-        # Store dimensions
-        self.nx = nx
-        self.ny = ny
-        self.nb = nx * ny
-
         # Set parameters from input or defaults
-        self.perm_field = kwargs.pop('perm_field', np.ones(self.nb) * 1000)
+        self.nx, self.ny = perm_field.shape
+        if self.nx != self.ny:
+            print("Warning, if nx!=ny result is probably wrong.")
+        self.size = perm_field.size
+        self.shape = perm_field.shape
+        self.np = (self.nx - 1) * self.ny + self.nx * (self.ny - 1)
+        self.perm_field = perm_field.ravel()
         self.phi = kwargs.pop('phi', 0.2)
         self.c_f = kwargs.pop('c_f', 1e-5)
         self.p0 = kwargs.pop('p0', 1)
@@ -50,28 +51,24 @@ class ReservoirSim:
         self.pres_inj = kwargs.pop('pres_inj', 180)
 
         # Store volumes : TODO : generalize to arb. volumes
-        self.volumes = np.ones(self.nb) * self.dx * self.dy * self.dz
+        self.volumes = np.ones(self.size) * self.dx * self.dy * self.dz
 
         # TODO : only necessary for well locations
         # Only depends on dx and dz, WHY?
         self.wi = 2 * np.pi * self.perm_field * self.dz
         self.wi /= self.mu_w * np.log(0.208 * self.dx / self.rw)
 
-        # TODO
-        self.nconn = (self.nx - 1) * self.ny + self.nx * (self.ny - 1)
-
     def get_matrix(self, Phi, compr, p):
         """Construct K-matrix."""
 
         # Pre-allocate diagonals.
-        mn = np.zeros(self.nb)
-        m1 = np.zeros(self.nb)
+        mn = np.zeros(self.size)
+        m1 = np.zeros(self.size)
         d = compr.copy()
-        p1 = np.zeros(self.nb)
-        pn = np.zeros(self.nb)
+        p1 = np.zeros(self.size)
+        pn = np.zeros(self.size)
 
         # v v TODO Adjust for nx!=ny TODO
-        # TODO FIRST: CHANGE PERM_FIELD to 2D array!
         t1 = self.dy * self.perm_field[:-1] * self.perm_field[1:]
         t1 /= self.perm_field[:-1] + self.perm_field[1:]
         t1 *= (Phi[:-1] + Phi[1:]) / 2
@@ -90,17 +87,18 @@ class ReservoirSim:
         pn[self.nx:] -= t2
 
         # Bring to sparse matrix
-        offsets = np.array([-self.ny, -1, 0, 1, self.nx])
+        # offsets = np.array([-self.ny, -1, 0, 1, self.nx])  # TODO
+        offsets = np.array([-self.nx, -1, 0, 1, self.nx])  # Gabriel's version
         data = np.array([mn, m1, d, p1, pn])
-        K = sp.sparse.dia_array((data, offsets), shape=(self.nb, self.nb))
+        K = sp.sparse.dia_array((data, offsets), shape=(self.size, self.size))
         return K.tocsc()
 
     def simulate(self, realizations=10, dt=0.0001):
 
         compr = self.volumes * self.phi * self.c_f / dt
 
-        P = np.empty((realizations+1, self.nb))
-        P[0, :] = np.ones(self.nb) * self.pres_ini
+        P = np.empty((realizations+1, self.size))
+        P[0, :] = np.ones(self.size) * self.pres_ini
 
         for i in range(realizations):
 
@@ -117,6 +115,6 @@ class ReservoirSim:
             K[-1, -1] += self.wi[-1]
 
             # Solve the system
-            P[i+1, :] = sp.sparse.linalg.spsolve(K, f)
+            P[i+1, :] = sp.sparse.linalg.spsolve(K, f, use_umfpack=False)
 
-        return P
+        return P.reshape((realizations+1, *self.shape))
